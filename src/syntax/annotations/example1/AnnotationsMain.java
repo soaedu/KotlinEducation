6d686f4d8a3906d86e6242b718dc345a15408306
soaedu <sergey.ostrovsky.edu@gmail.com>
2021-08-31 10:56:25 +0300

    Add new annotation examples.

--- src/syntax/annotations/example1/AnnotationsMain.java
+++ src/syntax/annotations/example1/AnnotationsMain.java
@@ -1,8 +1,6 @@
 package syntax.annotations.example1;
 
-// import syntax.annotations.example1.strings.JoinKt;
 import syntax.annotations.example1.strings.*;
-
 import java.util.ArrayList;
 
 public class AnnotationsMain {
@@ -12,23 +10,26 @@ public class AnnotationsMain {
         list.add(2);
         list.add(3);
 
-        // #1
+        // #1 no annotation
         // Вызов статического метода из класса JoinKt (до использования аннотации)
         // String joinedString = JoinKt.joinToString(list ,", ","", "");
 
-        // #2
+        // #2 with annotation
         // Изменение имени класса в файпе.
         // Чтобы изменить имя класса с КоtLin-функциями верхнего уровня, нужно добавить в
         // файл аннотацию @JvmName.
         // Поместите её в начало файла перед именем пакета:
-        // @file: JvmName("StringFunctions")                <- Аннотация дnя объямения имени класса
-        // package strings                                  <- Выражение package следует за аннотациями уровня файла
-        // fun joinToString( . . . ): String { ... }
-        // Теперь функцию можно вызвать так:
-        /* Java */
-        // import strings.StringFunctions;
-        // StringFunctions.joinToString(list ,", ","", "");
-        String joinedString = StringFunctions.joinToString(list ,", ","", "");
+        // String joinedString = StringFunctions.joinToString(list,", ","", "");
+
+        // #3
+        // Чтобы вызвать из Java функцию, написанную на Kotlin и имеющую аргументы с значениями по-умолчанию,
+        // нужно указать значения для всех аргументов.
+        // String joinedString = StringFunctions.joinToString(list,", ","", "");
+
+        // #4
+        // Чтобы вызвать из Java функцию, написанную на Kotlin и имеющую аргументы с значениями по-умолчанию,
+        // и не указывать значения для всех этих аргументов, метод нужно предварить аннотацией @JVMOverloads
+        String joinedString = StringFunctions.joinToString(list);
 
         // print ArrayList
         System.out.println("ArrayList : " + list + "\njoinedString: " +joinedString);
