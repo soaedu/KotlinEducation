bfb07f7cd7a42b379fe78252c586d91351429aad
soaedu <sergey.ostrovsky.edu@gmail.com>
2021-09-06 12:31:28 +0300

    Add new lateinit example.

--- src/syntax/control_flow/keyword/lateinit/LateinitMain.kt
+++ src/syntax/control_flow/keyword/lateinit/LateinitMain.kt
@@ -4,9 +4,13 @@ fun main(args: Array<String>) {
     val test = SqlTests()
     test.dbConnType = "MySql"
 
-    println("\nConnection to: ${test.dbConnType}")
+    if (test.hasValue()) {
+        println("\nConnection to: ${test.dbConnType}")
+    }
 }
 
 class SqlTests {
     lateinit var dbConnType: String
+
+    fun hasValue() = this::dbConnType.isInitialized
 }
