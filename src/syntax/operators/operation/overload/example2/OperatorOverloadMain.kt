b9528df2e44b9a04bcf8a83286f7a2a82670fcb5
soaedu <sergey.ostrovsky.edu@gmail.com>
2021-09-07 17:39:36 +0300

    Add new operator overload example.

--- src/syntax/operators/operation/overload/example2/OperatorOverloadMain.kt
+++ src/syntax/operators/operation/overload/example2/OperatorOverloadMain.kt
@@ -26,6 +26,21 @@ fun main() {
     list += 3
     val newList = list + listOf(4, 5)
     println("3. \tlist:\t$list \n\tnewList:$newList")
+
+    val list1 = listOf(1, 2, 3)
+    var list2 = list1
+    list2 += 4                                          // new list will be created
+    println("4. \tlist1:\t$list1 \n\tlist2:$list2")
+
+    var list3 = listOf(1, 2, 3)
+    val list4 = list3
+    list3 += 4                                          // new list will be created
+    println("5. \tlist3:\t$list3 \n\tlist4:$list4")
+
+    val list5 = mutableListOf(1, 2, 3)
+    val list6 = list5
+    list5 += 4
+    println("6. \tlist5:\t$list5 \n\tlist6:$list6")     // existing list will extend
 }
 
 /**
