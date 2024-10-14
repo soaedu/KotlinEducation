6b1e4bb3732e4032195f9a4ebe3f4256b68a5c72
Sergey Ostrovsky <sergey.ostrovsky.edu@gmail.com>
2024-10-14 13:03:19 +0200

    Add example to data/type/standard_type/collection_type/operation/last_operation

---
+++ src/main/kotlin/data/type/standard_type/collection_type/operation/last_operation/Main.kt
@@ -0,0 +1,6 @@
+package data.type.standard_type.collection_type.operation.last_operation
+
+fun main() {
+    val intArray = intArrayOf(1, 2, 3)
+    println("Show last item of the 'intArray': ${intArray.last()}")
+}
