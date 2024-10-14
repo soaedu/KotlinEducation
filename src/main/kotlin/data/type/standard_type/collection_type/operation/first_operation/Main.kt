1e6e10d9702c40b29739be490c3c55e2a948e53e
Sergey Ostrovsky <sergey.ostrovsky.edu@gmail.com>
2024-10-14 13:01:40 +0200

    Add example to data/type/standard_type/collection_type/operation/first_operation

---
+++ src/main/kotlin/data/type/standard_type/collection_type/operation/first_operation/Main.kt
@@ -0,0 +1,6 @@
+package data.type.standard_type.collection_type.operation.first_operation
+
+fun main() {
+    val intArray = intArrayOf(1, 2, 3)
+    println("Show first item of the 'intArray': ${intArray.first()}")
+}
