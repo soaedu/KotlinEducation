49bab6dbacec8b3782746d3881cda701c79ef2a7
Sergey Ostrovsky <sergey.ostrovsky.edu@gmail.com>
2024-10-14 13:06:44 +0200

    Add comments to examples

--- src/main/kotlin/data/type/standard_type/collection_type/operation/first_operation/Main.kt
+++ src/main/kotlin/data/type/standard_type/collection_type/operation/first_operation/Main.kt
@@ -1,5 +1,8 @@
 package data.type.standard_type.collection_type.operation.first_operation
 
+/**
+ * Method 'first()' is an extension method that returns the first element in the collection.
+ */
 fun main() {
     val intArray = intArrayOf(1, 2, 3)
     println("Show first item of the 'intArray': ${intArray.first()}")
--- src/main/kotlin/data/type/standard_type/collection_type/operation/last_operation/Main.kt
+++ src/main/kotlin/data/type/standard_type/collection_type/operation/last_operation/Main.kt
@@ -1,5 +1,8 @@
 package data.type.standard_type.collection_type.operation.last_operation
 
+/**
+ * Method 'last()' is an extension method that returns the last element in the collection.
+ */
 fun main() {
     val intArray = intArrayOf(1, 2, 3)
     println("Show last item of the 'intArray': ${intArray.last()}")
