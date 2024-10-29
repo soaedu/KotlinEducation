544edbf5c02bb7fc4c95bd427acf18e1a0373324
Sergey Ostrovsky <sergey.ostrovsky.edu@gmail.com>
2024-10-29 11:21:32 +0200

    Move examples of lambda function to functional programming folder

--- src/main/kotlin/syntax/functional_programming/function_type/lambda_func/example_4/Main.kt
+++ src/main/kotlin/syntax/functional_programming/function_type/lambda_func/example_4/Main.kt
@@ -1,4 +1,4 @@
-package syntax.functional_programming.function_type.anonymous_func.lambda_func.example_4
+package syntax.functional_programming.function_type.lambda_func.example_4
 
 fun main() {
     val priceBeforeDiscount = 153.25
