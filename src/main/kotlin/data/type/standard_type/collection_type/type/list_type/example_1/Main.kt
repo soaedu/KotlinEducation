57ac2b3c8ef8045d2ad8ded91216554ef868d8f9
Sergey Ostrovsky <sergey.ostrovsky.edu@gmail.com>
2024-10-29 11:27:29 +0200

    Add missed 'e' letter to comments

--- src/main/kotlin/data/type/standard_type/collection_type/type/list_type/example_1/Main.kt
+++ src/main/kotlin/data/type/standard_type/collection_type/type/list_type/example_1/Main.kt
@@ -5,24 +5,24 @@ import data.helpers.printItems
 fun main() {
     // Read-only instances
     // #1
-    // Use 'List()' to crate a read-only list.
+    // Use 'List()' to create a read-only list.
     val ints_1 = List(10) { it + 1 }
     // #2
-    // Use 'listOf()' to crate a read-only list.
+    // Use 'listOf()' to create a read-only list.
     val ints_2: List<Int> = listOf(20, 29, 40, 10)
     // #3
-    // Use 'emptyList()' to crate a read-only list.
+    // Use 'emptyList()' to create a read-only list.
     val emptyList: List<String> = emptyList()
     // #4
-    // Use 'listOfNotNull()' to crate a read-only list.
+    // Use 'listOfNotNull()' to create a read-only list.
     val notNulls: List<String> = listOfNotNull(null, "a", "b", "c")
 
     // Mutable instances
     // #5
-    // Use 'arrayListOf()' to crate a mutable list.
+    // Use 'arrayListOf()' to create a mutable list.
     val doubles: ArrayList<Double> = arrayListOf(84.88, 100.25, 999.99)
     // #6
-    // Use 'mutableListOf' to crate a mutable list.
+    // Use 'mutableListOf' to create a mutable list.
     val cartoons: MutableList<String> = mutableListOf(
         "Tom&Jerry",
         "Dexter's Laboratory",
